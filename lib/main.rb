7c0cfd8ae70428b6aa83ede1d3fcc755820db829
shoppai-shoochan <shoppai.shoochan@gmail.com>
2018-09-05 06:42:06 +0900

    Paralellでスクレイピング部分を並列化

--- lib/main.rb
+++ lib/main.rb
@@ -12,16 +12,16 @@ articles = {}
 #サイトインスタンスを格納する配列
 sites = []
 #urlの最後は/で終わる
-#sites << Nikkei.new("https://www.nikkei.com/","日経新聞")
-#sites << Sankei.new("https://www.sankei.com/","産経新聞")
+sites << Nikkei.new("https://www.nikkei.com/","日経新聞")
+sites << Sankei.new("https://www.sankei.com/","産経新聞")
 sites << Yomiuri.new("https://www.yomiuri.co.jp/","読売新聞")
-#sites << Tokyo.new("http://www.tokyo-np.co.jp/","東京新聞")
-#sites << Asahi.new("https://www.asahi.com/","朝日新聞")
-#sites << Mainichi.new("https://mainichi.jp/","毎日新聞")
-#sites << Chunichi.new("http://www.chunichi.co.jp/","中日新聞")
-#sites << Tosupo.new("https://www.tokyo-sports.co.jp/","東京スポーツ")
+sites << Tokyo.new("http://www.tokyo-np.co.jp/","東京新聞")
+sites << Asahi.new("https://www.asahi.com/","朝日新聞")
+sites << Mainichi.new("https://mainichi.jp/","毎日新聞")
+sites << Chunichi.new("http://www.chunichi.co.jp/","中日新聞")
+sites << Tosupo.new("https://www.tokyo-sports.co.jp/","東京スポーツ")
 
-sites.each{|site|
+Parallel.each(sites,in_threads: 4){|site|
   articles[site.site_name] = site.scraping
 }
 
