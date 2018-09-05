4f109b2738717d4e3ef10f5ba909b9e202fec8b8
shoppai-shoochan <shoppai.shoochan@gmail.com>
2018-09-06 02:38:27 +0900

    htmlの空白・改行対策

--- lib/write_html.rb
+++ lib/write_html.rb
@@ -5,11 +5,11 @@ def write_html(wordcounts,wordarticles)
 
   #erbテンプレートファイルを読み込み
   file = open("../view/Order_by_trend.html.erb","r")
-  erb = ERB.new(file.read)
+  erb = ERB.new(file.read,nil,'-')
 
   #erb.resultメソッドでhtmlに変換
   #f.putsでファイルに出力
-	#bindingはerbライブラリ側の変数、使用するとerb内のスコープがローカル(write_html)内になる？
+	#bindingはerbライブラリ側の変数、使用するとerb.result内のスコープがローカル(write_html)と一緒になる？
   File.open("../Trendword.html","w"){|f|
     f.puts(erb.result(binding))
   }
